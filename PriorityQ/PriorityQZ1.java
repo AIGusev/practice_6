641150f7222ce0d882937160c1a89d7bbd3f3855
AIGusev <artemgusevjob94@gmail.com>
2026-03-01 18:58:39 +0300

    Update PriorityQZ1.java

--- PriorityQ/PriorityQZ1.java
+++ PriorityQ/PriorityQZ1.java
@@ -6,14 +6,6 @@ public class PriorityQZ1 {
     static void main(String[] args) {
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
 
-        /*priorityQueue.add(8);
-        priorityQueue.add(5);
-        priorityQueue.add(1);
-        priorityQueue.add(9);
-        priorityQueue.add(7);
-        System.out.println(priorityQueue);*/
-        //Немного не понимаю, почему он делает странный порядок. 1, 7, 5, 9, 8
-
         priorityQueue.offer(10);
         priorityQueue.offer(8);
         priorityQueue.offer(1);
@@ -23,8 +15,7 @@ public class PriorityQZ1 {
         while (!priorityQueue.isEmpty())
             System.out.println(priorityQueue.poll());
 
-        //Тут тоже странный порядок
-
     }
 }
+
 
